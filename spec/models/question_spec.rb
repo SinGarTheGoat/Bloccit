ccfe926ba43b15f84fdc447b79ace379f4072a53
Forrest Stone <iloveburningman@gmail.com>
2016-05-18 16:36:44 -0700

    mid work upload

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.create! title: "New Question Title", body: "New Question Body", resolved:false }#had resolved:false
+
+  describe "attributes" do
+    it "it has a title body and a booleian attributes" do
+    expect(question).to have_attributes(title: "New Question Title", body: "New Question Body", resolved:false )
+  end#had resolved:false
+  end
+
+
+
+
+
+end
