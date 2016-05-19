719e9cb254ab8c481327eaf965a87f0958e1a682
Forrest Stone <iloveburningman@gmail.com>
2016-05-19 11:42:31 -0700

    Assignment 30 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,33 @@ require 'random_data'
 
 # Create Posts
 50.times do
-# #1
-  Post.create!(
-# #2
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-  )
+    # #1
+    Post.create!(
+        # #2
+        title:  RandomData.random_sentence,
+        body:   RandomData.random_paragraph
+    )
 end
+Post.find_or_create_by(
+    title: 'Yaaaaa Im some random title',
+    body: 'Yaaaaa Im some random body'
+
+)
 posts = Post.all
 
 # Create Comments
 # #3
 100.times do
-  Comment.create!(
-# #4
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
+    Comment.create!(
+        # #4
+        post: posts.sample,
+        body: RandomData.random_paragraph
+    )
 end
 
-puts "Seed finished"
+Comment.find_or_create_by(
+  body: 'Yaaaaa Im some random body'
+)
+puts 'Seed finished'
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
