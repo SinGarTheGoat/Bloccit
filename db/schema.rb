02760d0235c0ecc5bcd54f74a0931b0c049cf77d
Forrest Stone <iloveburningman@gmail.com>
2016-05-23 09:53:47 -0700

    Checkpoint 32 CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20160520235803) do
 
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -28,6 +37,14 @@ ActiveRecord::Schema.define(version: 20160520235803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "quote"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
