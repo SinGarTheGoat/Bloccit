621cb9672bce6ec92c1bb62d21658d60382e99cc
Forrest Stone <iloveburningman@gmail.com>
2016-05-31 16:53:32 -0700

    checkpoint 43 voting done

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ActiveRecord::Base
      has_many :labelings, as: :labelable
      has_many :labels, through: :labelings
      has_many :votes, dependent: :destroy
+
+     after_create :create_vote
+
      default_scope { order('rank DESC') }
      validates :title, length: { minimum: 5 }, presence: true
      validates :body, length: { minimum: 20 }, presence: true
@@ -29,6 +32,10 @@ class Post < ActiveRecord::Base
        age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
        new_rank = points + age_in_days
        update_attribute(:rank, new_rank)
+     end
+
+     def create_vote
+       user.votes.create(value: 1, post: self)
      end
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -72,6 +72,39 @@ RSpec.describe Post, type: :model do
               end
 
             end
-          end 
+          end
+          it "updates the rank when an up vote is created" do
+                old_rank = post.rank
+                post.votes.create!(value: 1)
+                expect(post.rank).to eq (old_rank + 1)
+              end
+
+              it "updates the rank when a down vote is created" do
+                old_rank = post.rank
+                post.votes.create!(value: -1)
+                expect(post.rank).to eq (old_rank - 1)
+              end
+            end
+            describe "#create_vote" do
+              it 'sets posts up votes eq to 1' do
+                expect(post.up_votes).to eq(1)
+              end
+              it 'calls #create_vote when a post is created' do
+                post = topic.post.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+                expect(post).to receive(:create_vote)
+                post.save
+              end
+              it 'associeates the vote with the owner of the post' do
+                expect(post.votes.first.user).to eq(post.user)
+              end
+
+
+
+            end
+
+
+
+
+
 
     end
