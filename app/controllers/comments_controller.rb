8c8e9794138320239ec5d8288c3a98058a68a812
Forrest Stone <iloveburningman@gmail.com>
2016-06-04 12:59:45 -0700

    Checkpoint 47 ajax DONE

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,49 +1,48 @@
 class CommentsController < ApplicationController
-  before_action :require_sign_in
-  before_action :authorize_user, only: [:destroy]
+    before_action :require_sign_in
+    before_action :authorize_user, only: [:destroy]
 
-  def create
-    # #11
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.new(comment_params)
-    comment.user = current_user
+    def create
+        # #11
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.new(comment_params)
+        @comment.user = current_user
+        @new_comment = Comment.new
 
-    if comment.save
-      flash[:notice] = 'Comment saved successfully.'
-      # #12
-      redirect_to [@post.topic, @post]
-    else
-      flash[:alert] = 'Comment failed to save.'
-      # #13
-      redirect_to [@post.topic, @post]
-    end
-  end
-
-  def destroy
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
-
-    if comment.destroy
-      flash[:notice] = 'Comment was deleted successfully.'
-      redirect_to [@post.topic, @post]
-    else
-      flash[:alert] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
-    end
+        if @comment.save
+            flash[:notice] = 'Comment saved successfully.'
+        else
+            flash[:alert] = 'Comment failed to save.'
+        end
     end
 
-  private
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        if @comment.destroy
+            flash[:notice] = 'Comment was deleted successfully.'
+        else
+            flash[:alert] = "Comment couldn't be deleted. Try again."
 
-  # #14
-  def comment_params
-    params.require(:comment).permit(:body)
-  end
+        end
+        respond_to do |format|
+            format.html
+            format.js
+        end
+      end
 
-  def authorize_user
-    comment = Comment.find(params[:id])
-    unless current_user == comment.user || current_user.admin?
-      flash[:alert] = 'You do not have permission to delete a comment.'
-      redirect_to [comment.post.topic, comment.post]
+    private
+
+    # #14
+    def comment_params
+        params.require(:comment).permit(:body)
     end
-   end
+
+    def authorize_user
+        comment = Comment.find(params[:id])
+        unless current_user == comment.user || current_user.admin?
+            flash[:alert] = 'You do not have permission to delete a comment.'
+            redirect_to [comment.post.topic, comment.post]
+        end
+     end
 end
