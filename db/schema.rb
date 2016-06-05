0963a17a2fdbebe667fc078891bd95c81ffbf3f4
Forrest Stone <iloveburningman@gmail.com>
2016-06-04 17:29:28 -0700

    Checkpoint 48 API Done

--- db/schema.rb
+++ db/schema.rb
@@ -11,84 +11,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_601_022_012) do
-  create_table 'comments', force: :cascade do |t|
-    t.text     'body'
-    t.integer  'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer  'user_id'
+ActiveRecord::Schema.define(version: 20160604222704) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
-  add_index 'comments', ['post_id'], name: 'index_comments_on_post_id'
-  add_index 'comments', ['user_id'], name: 'index_comments_on_user_id'
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
-  create_table 'favorites', force: :cascade do |t|
-    t.integer  'user_id'
-    t.integer  'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index 'favorites', ['post_id'], name: 'index_favorites_on_post_id'
-  add_index 'favorites', ['user_id'], name: 'index_favorites_on_user_id'
+  add_index "favorites", ["post_id"], name: "index_favorites_on_post_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
-  create_table 'labelings', force: :cascade do |t|
-    t.integer  'label_id'
-    t.integer  'labelable_id'
-    t.string   'labelable_type'
-    t.datetime 'created_at',     null: false
-    t.datetime 'updated_at',     null: false
+  create_table "labelings", force: :cascade do |t|
+    t.integer  "label_id"
+    t.integer  "labelable_id"
+    t.string   "labelable_type"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  add_index 'labelings', ['label_id'], name: 'index_labelings_on_label_id'
-  add_index 'labelings', %w(labelable_type labelable_id), name: 'index_labelings_on_labelable_type_and_labelable_id'
+  add_index "labelings", ["label_id"], name: "index_labelings_on_label_id"
+  add_index "labelings", ["labelable_type", "labelable_id"], name: "index_labelings_on_labelable_type_and_labelable_id"
 
-  create_table 'labels', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "labels", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string   'title'
-    t.text     'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string   'quote'
-    t.integer  'topic_id'
-    t.integer  'user_id'
-    t.float    'rank'
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "quote"
+    t.integer  "topic_id"
+    t.integer  "user_id"
+    t.float    "rank"
   end
 
-  add_index 'posts', ['topic_id'], name: 'index_posts_on_topic_id'
-  add_index 'posts', ['user_id'], name: 'index_posts_on_user_id'
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table 'topics', force: :cascade do |t|
-    t.string   'name'
-    t.boolean  'public', default: true
-    t.text     'description'
-    t.datetime 'created_at',                 null: false
-    t.datetime 'updated_at',                 null: false
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name'
-    t.string   'email'
-    t.string   'password_digest'
-    t.datetime 'created_at',      null: false
-    t.datetime 'updated_at',      null: false
-    t.integer  'role'
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "role"
+    t.string   "auth_token"
   end
 
-  create_table 'votes', force: :cascade do |t|
-    t.integer  'value'
-    t.integer  'user_id'
-    t.integer  'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index 'votes', ['post_id'], name: 'index_votes_on_post_id'
-  add_index 'votes', ['user_id'], name: 'index_votes_on_user_id'
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
+
 end
